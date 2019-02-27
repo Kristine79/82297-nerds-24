a3ad9f80a5e94d6e8565e3b4da23632f06602b5a
Kristine <magic.rooter@gmail.com>
2019-02-27 17:13:09 +0300

    финальные правки
    проверяла по базовым и дополнительным критериям

---
+++ js/main.js
@@ -0,0 +1,44 @@
+  var contactlink = document.querySelector('.modal-write-us');
+  var popup = document.querySelector('.contact-form');
+  var close = popup.querySelector('.contact-close');
+  var name = popup.querySelector('.name');
+  var form = popup.querySelector('form');
+  var email = popup.querySelector('.email');
+  var isStorageSupport = true;
+  var storage = "";
+  try {
+    storage = localStorage.getItem('name');
+  } catch (err) {
+    isStorageSupport = false;
+  }
+  contactlink.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    popup.classList.add('contact-show');
+  });
+  close.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    popup.classList.remove('contact-show');
+    popup.classList.remove('contact-error');
+    popup.offsetWidth = popup.offsetWidth;
+    if (storage) {
+      name.value = storage;
+      email.focus();
+    } else {
+      name.focus();
+    }
+    });
+  form.addEventListener('submit', function (evt) {
+    if (!name.value || !email.value) {
+      evt.preventDefault();
+      popup.classList.add('contact-error');
+    } else if (isStorageSupport);
+  });
+  window.addEventListener('keydown', function (evt) {
+     if (evt.keyCode === 27 && popup.classList.contains("contact-show")) {
+      evt.preventDefault();
+      if (popup.classList.contains('contact-show')) {
+        popup.classList.remove('contact-show');
+        popup.classList.remove('contact-error');
+      }
+    }
+  });
