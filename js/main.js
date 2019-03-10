6a3c4243e91eaade02cbe9f786d3a9bf636ce47b
Kristine <magic.rooter@gmail.com>
2019-03-10 12:43:45 +0300

    правки, надеюсь последние)

--- js/main.js
+++ js/main.js
@@ -1,44 +1,61 @@
-  var contactlink = document.querySelector('.modal-write-us');
-  var popup = document.querySelector('.contact-form');
-  var close = popup.querySelector('.contact-close');
-  var name = popup.querySelector('.name');
-  var form = popup.querySelector('form');
-  var email = popup.querySelector('.email');
-  var isStorageSupport = true;
-  var storage = '';
-  try {
-    storage = localStorage.getItem('name');
-  } catch (err) {
-    isStorageSupport = false;
-  }
-  contactlink.addEventListener('click', function (evt) {
+var contactlink = document.querySelector('.modal-write-us');
+var popup = document.querySelector('.contact-form');
+var form = popup.querySelector('.form');
+var close = popup.querySelector('.contact-close');
+var inputName = popup.querySelector('.name');
+var textarea = popup.querySelector('.letter-text');
+var email = popup.querySelector('.email');
+
+contactlink.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  popup.classList.add('contact-show');
+});
+close.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  popup.classList.remove('contact-show');
+  popup.classList.remove('contact-error');
+  popup.offsetWidth = popup.offsetWidth;
+});
+form.addEventListener('submit', function (evt) {
+  if (!inputName.value || !email.value || !textarea.value) {
     evt.preventDefault();
-    popup.classList.add('contact-show');
-  });
-  close.addEventListener('click', function (evt) {
+    popup.classList.add('contact-error');
+  }
+  if (!inputName.value ) {
+    inputName.classList.add('error');
+  }
+  if (!email.value ) {
+    email.classList.add('error');
+  }
+  if (!textarea.value ) {
+    textarea.classList.add('error');
+  }
+});
+inputName.addEventListener('input', function() {
+  if (!inputName.value ) {
+    inputName.classList.add('error');
+  } else {
+    inputName.classList.remove('error');
+  }
+});
+email.addEventListener('input', function() {
+  if (!email.value ) {
+    email.classList.add('error');
+  } else {
+    email.classList.remove('error');
+  }
+});
+textarea.addEventListener('input', function() {
+  if (!textarea.value ) {
+    textarea.classList.add('error');
+  } else {
+    textarea.classList.remove('error');
+  }
+});
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27 && popup.classList.contains('contact-show')) {
     evt.preventDefault();
     popup.classList.remove('contact-show');
     popup.classList.remove('contact-error');
-    popup.offsetWidth = popup.offsetWidth;
-    if (storage) {
-      name.value = storage;
-      email.focus();
-    } else {
-      name.focus();
-    }
-  });
-  form.addEventListener('submit', function (evt) {
-    if (!name.value || !email.value) {
-      evt.preventDefault();
-      popup.classList.add('contact-error');
-    } else if (isStorageSupport);
-      localStorage.setItem("login", login.value);
-  });
-  window.addEventListener('keydown', function (evt) {
-     if (evt.keyCode === 27 && popup.classList.contains('contact-show')) {
-      evt.preventDefault();
-        popup.classList.remove('contact-show');
-        popup.classList.remove('contact-error');
-      }
-
-  });
+  }
+});
